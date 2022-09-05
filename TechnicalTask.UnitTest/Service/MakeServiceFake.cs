d2531755295af23ec2cc5e1909b25a281c6d02c5
kumara.ks <kumara.ks@verticurl.com>
2022-09-05 15:21:22 +0530

    Unit tests added

---
+++ TechnicalTask.UnitTest/Service/MakeServiceFake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTask.Data.Models;
+using TechnicalTask.Service.Interface;
+
+namespace TechnicalTask.UnitTest.Service
+{
+    public class MakeServiceFake : IMake
+    {
+        private readonly List<Make> makes = new List<Make>();
+        public MakeServiceFake()
+        {
+            makes.Add(new Make()
+            {
+                Name = "Test item1"
+            }
+            );
+            makes.Add(new Make()
+            {
+                Name = "Test item2"
+            }
+            );
+
+        }
+        public Task<Make> AddMake(Make make)
+        {
+            makes.Add(make);
+            make.Id = makes.Count;
+            return new Task<Make>(() => make);
+        }
+        public IEnumerable<Make> GetMakes()
+        {
+            return makes;
+        }
+    }
+}
---
+++ TechnicalTask.UnitTest/Tests/MakeControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTask.Controllers;
+using TechnicalTask.Data.Models;
+using TechnicalTask.Service.Interface;
+using TechnicalTask.UnitTest.Service;
+using Xunit;
+using System.Linq;
+
+namespace TechnicalTask.UnitTest.Tests
+{
+    public class MakeControllerTest
+    {
+
+        private readonly MakeController _controller;
+        private readonly IMake _service;
+        public MakeControllerTest()
+        {
+            _service = new MakeServiceFake();
+            _controller = new MakeController(_service);
+        }
+
+        [Fact]
+        public void Add_ValidObjectPassed_ReturnsCreatedObject()
+        {
+            // Arrange
+            Make testItem = new Make()
+            {
+                Name = "Testing add item"
+            };
+            // Act
+            var createdResponse = _controller.AddMake(testItem);
+            // Assert
+            Assert.NotEqual(0, createdResponse.Id);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+
+            // Act
+            var result = _controller.GetMakes();
+            // Assert
+            var items  = (JsonConvert.DeserializeObject<List<Make>>(result.ToString()));
+            Assert.Equal(2, items.Count);
+        }
+    }
+}
